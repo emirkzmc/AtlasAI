4525e571930e93f006b742cb4579e3be609b4b1a
Emir Yusuf Kazmacı <emiryusfkaz48@gmail.com>
2026-05-02 12:56:04 +0300

    chore: load firebase config from env variables

--- src/firebase/firebase.config.ts
+++ src/firebase/firebase.config.ts
@@ -1,21 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCmkwTb1F8xkpnkP8B0ybSvUKOYqcEV6Fo",
-  authDomain: "student-portal-6b871.firebaseapp.com",
-  projectId: "student-portal-6b871",
-  storageBucket: "student-portal-6b871.firebasestorage.app",
-  messagingSenderId: "833150287787",
-  appId: "1:833150287787:web:99a3600df0d20ed9cd5cee",
-  measurementId: "G-K95LVLWX62"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const analytics = typeof window !== "undefined" ? getAnalytics(app) : null;
